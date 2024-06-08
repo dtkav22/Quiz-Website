fdac8ef491b1f9679f227c79c943ce4ca98b4087
Mate Zhorzholiani <zhorzholianimate@gmail.com>
2024-06-08 21:39:27 +0400

    fixing typo

--- Quiz/TaskTypes.java
+++ Quiz/TaskTypes.java
@@ -5,5 +5,5 @@ public class TaskTypes {
     public static final int QUESTION_RESPONSE = 0;
     public static final int FILL_BLANK = 1;
     public static final int MULTIPLE_CHOICE = 2;
-    public static final int PICTURE_RESPONCE = 3;
+    public static final int PICTURE_RESPONSE = 3;
 }
