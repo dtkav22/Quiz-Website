069f1297034031e68b27100f3ee8ce79d55e2a92
Dimitri Tkavadze <dtkav22@freeuni.edu.ge>
2024-06-28 17:34:55 +0400

    implemented createQuiz doPost method

--- src/main/java/Servlets/createQuiz/createQuiz.java
+++ src/main/java/Servlets/createQuiz/createQuiz.java
@@ -1,5 +1,6 @@
 package Servlets.createQuiz;
 
+import Quiz.Quiz;
 import Quiz.QuizDAO;
 import Quiz.QuizTask;
 
@@ -20,6 +21,14 @@ public class createQuiz extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         QuizDAO quizDAO = new QuizDAO();
-        response.sendRedirect("/welcome.jsp");
+        HttpSession session = request.getSession();
+        String author_id = (String) session.getAttribute("userId");
+        ArrayList<QuizTask> tasks = (ArrayList<QuizTask>) session.getAttribute("Tasks");
+        String authorDescription = request.getParameter("quizDescription");
+        java.sql.Date creationDate = new java.sql.Date(System.currentTimeMillis());
+        boolean randomizeTasks = request.getParameter("tasksOrder").equals("RandomizedOrder");
+        boolean onMultiplePage = request.getParameter("quizAppearence").equals("MultiplePage");
+        quizDAO.addQuiz(new Quiz(author_id, tasks, authorDescription, creationDate, randomizeTasks, onMultiplePage));
+        response.sendRedirect("/createQuiz/QuizAddedSuccessfully.html");
     }
 }
