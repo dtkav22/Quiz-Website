309f28b804c735da23db79c61253b065bdcce5ef
Mate Zhorzholiani <zhorzholianimate@gmail.com>
2024-07-02 00:50:35 +0400

    adding quiz name

--- src/main/java/Quiz/Quiz.java
+++ src/main/java/Quiz/Quiz.java
@@ -11,16 +11,19 @@ public class Quiz {
     private final boolean randomizeTasks;
     private final boolean onMultiplePage;
     private final java.sql.Date creationDate;
+    private final String quiz_name;
 
-    public Quiz(String author_id, ArrayList<QuizTask> tasks, String authorDescription, java.sql.Date creationDate, boolean randomizeTasks, boolean onMultiplePage) {
+    public Quiz(String author_id, ArrayList<QuizTask> tasks, String authorDescription, java.sql.Date creationDate, boolean randomizeTasks, boolean onMultiplePage, String quizName) {
         this.tasks = tasks;
         this.author_id = author_id;
         this.randomizeTasks = randomizeTasks;
         this.onMultiplePage = onMultiplePage;
         this.authorDescription = authorDescription;
         this.creationDate = creationDate;
+        quiz_name = quizName;
     }
-    public Quiz(String author_id, QuizTask task, String authorDescription, java.sql.Date creationDate, boolean randomizeTasks, boolean onMultiplePage) {
+    public Quiz(String author_id, QuizTask task, String authorDescription, java.sql.Date creationDate, boolean randomizeTasks, boolean onMultiplePage, String quizName) {
+        quiz_name = quizName;
         tasks = new ArrayList<>();
         tasks.add(task);
         this.author_id = author_id;
@@ -29,8 +32,9 @@ public class Quiz {
         this.authorDescription = authorDescription;
         this.creationDate = creationDate;
     }
-    public Quiz(String author_id, String authorDescription, java.sql.Date creationDate, boolean randomizeTasks, boolean onMultiplePage) {
+    public Quiz(String author_id, String authorDescription, java.sql.Date creationDate, boolean randomizeTasks, boolean onMultiplePage, String quizName) {
         this.author_id = author_id;
+        quiz_name = quizName;
         tasks = new ArrayList<>();
         this.randomizeTasks = randomizeTasks;
         this.onMultiplePage = onMultiplePage;
